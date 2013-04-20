d54bc4cd46e63f1833dd3041b02ae0e64dad009f
Dreamo <VKruzliak@gmail.com>
2013-04-20 04:56:36 -0700

    added frame for little squares in gui

--- mywidget.cpp
+++ mywidget.cpp
@@ -9,25 +9,31 @@ MyWidget::MyWidget()
 {
 
 
+
+    qp.color().black();
+    qb.color().light();
     m_button = new QPushButton("My Button", this);
-        // set size and location of the button
-        m_button->setGeometry(QRect(QPoint(500, 500),
-                                     QSize(200, 50)));
-        connect(m_button, SIGNAL(released()), this, SLOT(handleButton()));
-        m_button->setText("Open FIle");
-    QPalette palette(MyWidget::palette());
-    palette.setColor(backgroundRole(), Qt::white);
-    setPalette(palette);
+    m_button->setGeometry(QRect(QPoint(500, 500),
+    QSize(200, 50)));
+    connect(m_button, SIGNAL(released()), this, SLOT(handleButton()));
+    m_button->setText("Open File");
+
+    map_bg = new QGraphicsView(this);
+    map_bg->setGeometry(50,50,200,200);
+    scene = new QGraphicsScene(map_bg);
+    scene->setSceneRect(50,50,200,200);
+    map_bg->setBackgroundBrush(QBrush(Qt::white, Qt::SolidPattern));
+    map_bg->setScene(scene);
+    map_bg->scale(1,-1);
+
 
 }
 
-void MyWidget::paintEvent(QPaintEvent *)
+void MyWidget::paint()
 {
     for (int u = 0; u<1500; u++)
         fff[u] = rand() % 8;
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing);
-    painter.setPen(Qt::darkGreen);
+
     int r = 0;
     int k = 0;
     for(int j = 0; j < 30; j++)
@@ -49,7 +55,9 @@ void MyWidget::paintEvent(QPaintEvent *)
         }
         r++;
 
-         painter.fillRect(50+i*6,50+j*6,5,5,c);
+        QGraphicsRectItem *r = scene->addRect(50+i*6,50+j*6,5,5,QPen(Qt::black),QBrush(c, Qt::SolidPattern));
+        //scene->mousePressEvent();
+        rec.push_back(r);
         }
     }
 
@@ -61,6 +69,7 @@ void MyWidget::handleButton()
     path = QFileDialog::getOpenFileName(this, tr("Open File"),"",tr("Files (*.*)"));
     Parser p;
     packets = p.getPacketsInfo(path);
+    paint();
 }
 
 MyWidget::~MyWidget()
